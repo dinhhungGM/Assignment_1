bfa5e546cb3cac1db53e3c623ef9616ea0f68e44
dinhhungGM <dinhhungGM@gmail.com>
2021-09-21 23:04:26 -0400

    add background, add add todo function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 console.log("Hello world");
+const addTodo = (e) => {
+  let inputTodo = $("#input-todo");
+  console.log(inputTodo);
+  $("#todo-item-list").append(`<div class="item"> ${inputTodo.val()}  </div>`);
+  inputTodo.val("");
+  
+};
